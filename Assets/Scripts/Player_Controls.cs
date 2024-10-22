81e66e1bc686bb324bf5cf53ad101aae1e4071d5
QuincifurCJones <qgerhardstein@stny.rr.com>
2024-10-22 10:13:08 -0400

    Stick COntrol working
    - make "working" stick control
    - added a jumping player

--- Assets/Scripts/Player_Controls.cs
+++ Assets/Scripts/Player_Controls.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Controls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputActionReference Right_Stick;
+    public InputActionReference Jump_button;
+    [SerializeField] public float jump_value = 1.0f;
+    [SerializeField] public float move_scale = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Right_Stick.action.performed += Move_player;
+        Jump_button.action.performed += jump;
+    }
+
+    void Move_player(InputAction.CallbackContext context)
+    {
+        Vector2 stick_input = context.ReadValue<Vector2>();
+
+        gameObject.transform.position += new Vector3(Math.Sign(stick_input.x) * move_scale, 0, Math.Sign(stick_input.y) * move_scale);
+    }
+
+    void jump(InputAction.CallbackContext context)
+    {
+        gameObject.transform.position += new Vector3(0, jump_value, 0);
     }
 }
--- Assets/Scripts/Stick_rotate.cs
+++ Assets/Scripts/Stick_rotate.cs
@@ -8,7 +8,8 @@ public class Stick_rotate : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject Island_enpty;
     public InputActionReference stick_move;
-    [SerializeField] int rotate_amount = 5;
+    [SerializeField] int rotate_amount_X = 5;
+    [SerializeField] int rotate_amount_Y = 5;
 
 
     // Update is called once per frame
@@ -17,8 +18,36 @@ public class Stick_rotate : MonoBehaviour
         stick_move.action.performed += Turn;
     }
 
-    void Turn(InputAction.CallbackContext __)
+    void Turn(InputAction.CallbackContext context)
     {
-        Island_enpty.transform.rotation *= Quaternion.Euler(0, rotate_amount, 0);
+        Vector2 stick_input = context.ReadValue<Vector2>();
+
+        int scale_rotate_x = 0;
+        int scale_rotate_y = 0;
+
+        // check X
+        if (stick_input.x >= 0.25f)
+        {
+            scale_rotate_x = 1;
+        }
+        else if (stick_input.x <= -0.25f)
+        {
+            scale_rotate_x = -1;
+        }
+
+        // check Y
+        if (stick_input.y >= 0.25f)
+        {
+            scale_rotate_y = 1;
+        }
+        else if (stick_input.y <= -0.25f)
+        {
+            scale_rotate_y = -1;
+        }
+
+        int let_y = 0;
+
+        Island_enpty.transform.rotation *= Quaternion.Euler(0,rotate_amount_X * scale_rotate_x, rotate_amount_Y * scale_rotate_y);
     }
+
 }
